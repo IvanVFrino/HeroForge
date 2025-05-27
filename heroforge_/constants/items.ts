758cb1652ef4f4eba5b2622fb38e050f881db9f5
IvanVFrino <73718122+IvanVFrino@users.noreply.github.com>
2025-05-27 19:41:08 -0300

    Delete heroforge_ directory

--- heroforge_/constants/items.ts
+++
@@ -1,5 +0,0 @@
-import { WeaponProperty, DamageType, ArmorType, WEAPON_PROPERTIES_CONST, DAMAGE_TYPES_CONST, ARMOR_TYPES_CONST } from '../types';
-
-export const WEAPON_PROPERTIES_LIST: readonly WeaponProperty[] = [...WEAPON_PROPERTIES_CONST];
-export const DAMAGE_TYPES_LIST: readonly DamageType[] = [...DAMAGE_TYPES_CONST];
-export const ARMOR_TYPES_LIST: readonly ArmorType[] = [...ARMOR_TYPES_CONST];
